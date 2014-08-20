b711c70fc8fdc178e960028118d07019f16f4ab2
V. SARAVANAN <saravanan.resume@gmail.com>
2014-08-21 01:05:21 +0800

    CalcJAXRS

---
+++ jersey/CalcJAXRS/src/main/java/com/theopentutorials/jaxrs/calc/CalcRESTStartUp.java
@@ -0,0 +1,24 @@
+package com.theopentutorials.jaxrs.calc;
+ 
+import java.io.IOException;
+import com.sun.jersey.api.container.httpserver.HttpServerFactory;
+import com.sun.net.httpserver.HttpServer;
+ 
+public class CalcRESTStartUp {
+ 
+    static final String BASE_URI = "http://localhost:9999/calcrest/";
+ 
+    public static void main(String[] args) {
+        try {
+            HttpServer server = HttpServerFactory.create(BASE_URI);
+            server.start();
+            System.out.println("Press Enter to stop the server. ");
+            System.in.read();
+            server.stop(0);
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
