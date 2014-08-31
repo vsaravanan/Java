57dc2d5774d113a4efd7e08f1c2fe7c9f1f7a3e0
vsaravanan <saravanan.resume@gmail.com>
2014-09-01 03:18:58 +0800

    upd

--- jersey/my-jersey-project/src/main/java/com/jd/router/EmpRouter.java
+++ jersey/my-jersey-project/src/main/java/com/jd/router/EmpRouter.java
@@ -4,6 +4,7 @@ import com.jd.exception.EmpNotFoundException;
 import com.jd.model.*;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
